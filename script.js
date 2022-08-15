b7bac5296adebe94128ae9121e7970d43792c226
Vaishali-Jaiswal <vaishalijaiswal117@gmail.com>
2022-08-15 11:40:14 +0530

    Adding ML Project

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function functionToGithub() {
-    window.location.href = "https://github.com/Vaishali-Jaiswal/Portfolio-Website";
-}
+burger = document.querySelector('.burger')
+navBar = document.querySelector('.h-nav')
+navList = document.querySelector('.v-class')
+
+burger.addEventListener('click', ()=> {
+    navList.classList.toggle('v-class');
+    navBar.classList.toggle('h-nav');
+})
+
+document.getElementById("githubP1").onclick = function () {
+    location.href = "https://github.com/Vaishali-Jaiswal/Portfolio-Website";
+};
+
+document.getElementById("githubP2").onclick = function () {
+    location.href = "https://github.com/Vaishali-Jaiswal/Machine-Learning-Project";
+};
